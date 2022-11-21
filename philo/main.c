5946414613a631e879ec7806041150453bf0078b
fesper-s <fafaeesfabricio@gmail.com>
2022-11-21 14:00:53 -0300

    added threads

--- philo/main.c
+++ philo/main.c
@@ -6,13 +6,13 @@
 /*   By: fesper-s <fesper-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 09:16:42 by fesper-s          #+#    #+#             */
-/*   Updated: 2022/11/18 16:42:51 by fesper-s         ###   ########.fr       */
+/*   Updated: 2022/11/21 10:40:36 by fesper-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	get_arg(t_philo *stats, char **str, int size)
+void	get_arg(t_data *data, char **str, int size)
 {
 	int	i;
 	int	j;
@@ -32,32 +32,70 @@ void	get_arg(t_philo *stats, char **str, int size)
 		}
 		i++;
 	}
-	stats->nbphilo = ft_atoi(str[1]);
-	stats->ttdie = ft_atoi(str[2]);
-	stats->tteat = ft_atoi(str[3]);
-	stats->ttsleep = ft_atoi(str[4]);
+	data->nbphilo = ft_atoi(str[1]);
+	data->ttdie = ft_atoi(str[2]);
+	data->tteat = ft_atoi(str[3]);
+	data->ttsleep = ft_atoi(str[4]);
 	if (size == 6)
-		stats->nbmeals = ft_atoi(str[5]);
+		data->nbmeals = ft_atoi(str[5]);
 }
 
-void	check_philo(t_philo *stats, int size)
+void	check_philo(t_data *data, int size)
 {
-	printf("Number of Philosophers: %d\n", stats->nbphilo);
-	printf("Time do die: %d\n", stats->ttdie);
-	printf("Time to eat: %d\n", stats->tteat);
-	printf("Time to sleep: %d\n", stats->ttsleep);
+	printf("Number of Philosophers: %d\n", data->nbphilo);
+	printf("Time do die: %d\n", data->ttdie);
+	printf("Time to eat: %d\n", data->tteat);
+	printf("Time to sleep: %d\n", data->ttsleep);
 	if (size == 6)
-		printf("Number of meals: %d\n", stats->nbmeals);
+		printf("Number of meals: %d\n", data->nbmeals);
+}
+
+void	*routine(void *p)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)p;
+	printf("ESTA PORRA %d\n", philo->id);
+	return (NULL);
+}
+
+void	attr_philo(t_data *data)
+{
+	int	i;
+
+	data->philo = malloc(sizeof(t_philo) * data->nbphilo);
+	i = -1;
+	while (++i < data->nbphilo)
+	{
+		data->philo[i].id = i;
+		data->philo[i].fork_l = i;
+		data->philo[i].fork_r = (i + 1) % data->nbphilo;
+	}
+	i = -1;
+	while (1)
+	{
+		while (++i < data->nbphilo)
+		{
+			pthread_create(&data->philo[i].threads, NULL, &routine, \
+				&data->philo[i]);
+		}
+		i = -1;
+		while (++i < data->nbphilo)
+		{
+			pthread_join(data->philo[i].threads, NULL);
+		}
+	}
 }
 
 int	main(int argc, char **argv)
 {
-	t_philo	stats;
+	t_data	data;
 
 	if (argc == 5 || argc == 6)
 	{
-		get_arg(&stats, argv, argc);
-		check_philo(&stats, argc);
+		get_arg(&data, argv, argc);
+		check_philo(&data, argc);
+		attr_philo(&data);
 	}
 	else
 		printf("Invalid number of arguments\n");
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: fesper-s <fesper-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 09:13:21 by fesper-s          #+#    #+#             */
-/*   Updated: 2022/10/31 09:28:25 by fesper-s         ###   ########.fr       */
+/*   Updated: 2022/11/21 10:38:10 by fesper-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,30 @@
 # include <stdlib.h>
 # include <pthread.h>
 
+typedef struct s_data
+{
+	int				nbphilo;
+	int				ttdie;
+	int				tteat;
+	int				ttsleep;
+	int				nbmeals;
+	pthread_mutex_t	*forks;
+	struct s_philo	*philo;
+}	t_data;
+
 typedef struct s_philo
 {
-	int	nbphilo;
-	int	ttdie;
-	int	tteat;
-	int	ttsleep;
-	int	nbmeals;
+	int			id;
+	int			fork_l;
+	int			fork_r;
+	pthread_t	threads;
+	t_data		*data;
 }	t_philo;
 
-//main.c
-void	get_arg(t_philo *stats, char **str, int size);
+// main.c
+void	get_arg(t_data *data, char **str, int size);
+void	*routine(void *p);
+void	attr_philo(t_data *data);
 // utils.c
 int		ft_isdigit(int n);
 int		ft_atoi(const char *str);
